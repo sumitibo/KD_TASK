c91dc2aacaa330e0f1dfd6b51f5b6ffb790959d0
Sumit Kumar <sumit.kumar@ibo.com>
2022-08-21 15:44:46 +0530

    Fs module

--- src/handlers/cart.handler.js
+++ src/handlers/cart.handler.js
@@ -1,99 +1,56 @@
-// // const data = require('../data/users.json');
-// // console.log(data.users);
+ //const fs = require("fs");
 
-// const fs = require("fs");
+//  fs.readFile("../data/users.json", "utf-8", function (err, users) {
+//    if (err) {
+//      console.log(err);
+//    } else {
+//      try {
+//        const data = JSON.parse(users);
+//        console.log(data.users);
+//      } catch (err) {
+//        console.log(err, "Error parsing users.json");
+//      }
+//    }
+//  });
 
-// // fs.readFile("../data/users.json", "utf-8", function (err, users) {
-// //   if (err) {
-// //     console.log(err);
-// //   } else {
-// //     try {
-// //       const data = JSON.parse(users);
-// //       console.log(data.users);
-// //     } catch (err) {
-// //       console.log(err, "Error parsing users.json");
-// //     }
-// //   }
-// // });
+//  let newer = {
+//    id: 2,
+//    name: "Sumit Doe",
+//    email: "john.doe@example.com",
+//    avatar: "https:example.com/avatar.png",
+//    phone: "123-456-1234",
+//    active: true,
+//    cart_id: null,
+//    cart_active: false,
+//  };
+//  function saver() {
+//    let newSaver = [];
+//    fs.readFile("../data/users.json", "utf-8", function (err, users) {
+//      if (err) {
+//        console.log(err);
+//      } else {
+//        try {
+//          const data = JSON.parse(users);
+//          console.log(data,"yhn se")
+//        } catch (err) {
+//          console.log(err, "Error parsing users.json");
+//        }
+//      }
+//    });
 
-// let newer = {
-//   id: 2,
-//   name: "Sumit Doe",
-//   email: "john.doe@example.com",
-//   avatar: "https://example.com/avatar.png",
-//   phone: "123-456-1234",
-//   active: true,
-//   cart_id: null,
-//   cart_active: false,
-// };
-// function saver() {
-//   let newSaver = [];
-//   fs.readFile("../data/users.json", "utf-8", function (err, users) {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       try {
-//         const data = JSON.parse(users);
-//         newSaver = [...data, newer];
-//         fs.writeFile(
-//           "../data/users.json",
-//           JSON.stringify(newSaver, null, 2),
-//           (err) => {
-//             if (err) {
-//               console.log(err);
-//             } else {
-//               console.log("File saved successfully");
-//             }
-//           }
-//         );
-//       } catch (err) {
-//         console.log(err, "Error parsing users.json");
-//       }
-//     }
-//   });
-
-//   // fs.writeFile("../data/users.json",JSON.stringify(newer,null,2),err =>{
-//   //     if(err) {
-//   //         console.log(err);
-//   //     }else{
-//   //         console.log("File saved successfully");
-//   //     }
-//   // })
-// }
-// saver();
+//     // fs.writeFile("../data/users.json",JSON.stringify(newer,null,2),err =>{
+//     //     if(err) {
+//     //         console.log(err);
+//     //     }else{
+//     //         console.log("File saved successfully");
+//     //     }
+//     // })
+//  }
+//  saver();
 const {generateCart} = require('../controllers/cart.controller');
+const {new_cart_schema} = require('../schemas/cart.schema');
 const create_new_cart = {
-  schema:{
-      params: {
-        type:"object",
-        properties:{
-          user_id:{
-            type:"string",
-            minLength:5
-          }
-        }
-      },
-      response:{
-        200:{
-          type:"object",
-          properties:{
-            token:{type:"string"}
-          }
-        },
-        201:{
-          type:"object",
-          properties:{
-            token:{type:"string"}
-          }
-        },
-        400:{
-          type:"object",
-          properties:{
-            error:{type:"string"}
-          }
-        }
-      }
-  },
+  schema:new_cart_schema,
   handler:generateCart
 }
 
