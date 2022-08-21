515fc3317b082258927aa95954e8003d3e28d9f7
Sumit Kumar <sumit.kumar@ibo.com>
2022-08-21 18:36:37 +0530

    Cart write enabled

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,27 +1,47 @@
+const { v4:uuidv4} = require("uuid");
 const fs = require("fs");
 const path = require("path");
 function generateCart(req, reply) {
   try {
-    const filePath = path.join(__dirname, "../data") + "/users.json";
-    const readFile = async () => {
-      try {
-        const data = await fs.promises.readFile(filePath, "utf8");
-        return data;
-      } catch (err) {
+    fs.readFile(path.join(__dirname, "../data")+ "/users.json","utf-8",(err, data)=>{
+      if(err) {
         console.log(err);
+      }else{
+        let users = JSON.parse(data);
+        let {user_id} = req.params; //geting the user id to get the exact user from the list;
+
+        let actual_user = users.filter((e)=>{
+          return e.id == user_id;
+        })
+        let cart
+        if(!actual_user.cart_active){
+          cart ={
+            cart_id:uuidv4(),
+            order_number: Math.floor(Math.random() * 1000000),
+            cart_lines: req.body.cart_line_id ? [req.body] : [],
+            user_id:user_id,
+          }
+          let data = JSON.stringify([cart], null, 2);
+          fs.writeFile(path.join(__dirname, "../data")+ "/cart.json",
+            data,
+            (err) => {
+              if (err) throw err;
+              console.log("Data written to file");
+            }
+          );
+        }
+        replyer(cart)
+        console.log(actual_user)
       }
-    };
-    readFile()
-      .then((res) => JSON.parse(res))
-      .then((res) => {
-        return reply.code(201).send({
-          order_number: "dhhdd",
-          res,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    });
+
+    function replyer(data){
+      return reply.code(201).send({
+      order_number:"dhhdd",
+      data
+    })
+    }
+    
   } catch (err) {
     return reply.code(400).send(err);
   }
--- src/schemas/cart.schema.js
+++ src/schemas/cart.schema.js
@@ -1,14 +1,47 @@
-const new_cart_schema ={
-    params: {
-      type:"object",
-      properties:{
-        user_id:{
-          type:"string",
-          minLength:1
-        }
-      }
+const cart_line = {
+  type: "object",
+  properties: {
+    quantity: {
+      type: "integer",
+      minimum: 1,
     },
-  
-}
+    offer_id: { type: "string" },
+    unit_price: {
+      type: "object",
+      properties: {
+        cent_amount: { type: "integer" },
+        currency: {
+          type: "string",
+          enum: ["INR"],
+        },
+        fraction: { type: "integer" },
+      },
+    },
+  },
+};
 
-module.exports ={new_cart_schema}
+const new_cart_schema = {
+  params: {
+    type: "object",
+    properties: {
+      user_id: {
+        type: "string",
+        minLength: 1,
+      },
+    },
+    required: ["user_id"],
+  },
+  body: cart_line,
+  // response: {
+  //   201: {
+  //     type: "object",
+  //     properties: {
+  //       cart_id: { type: "string", format: "uuid" },
+  //       order_number: { type: "string" },
+  //       user_id: { type: "string" },
+  //     },
+  //   },
+  // },
+};
+
+module.exports = { new_cart_schema };
