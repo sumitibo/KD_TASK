1bb83e206d2e8ec8b0cf35ed5f091ab3a72f7b9b
Sumit Kumar <sumit.kumar@ibo.com>
2022-08-21 16:30:45 +0530

    Fs module working

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,21 +1,30 @@
 const fs = require("fs");
 const path = require("path");
-let readStream = fs.createReadStream(
-  path.join(__dirname, "../data") + "/users.json",
-  "utf8"
-);
-
-async function generateCart(req, reply) {
+function generateCart(req, reply) {
   try {
-    let data = "";
-    readStream
-      .on("data", function (chunk) {
-        data += chunk;
+    const filePath = path.join(__dirname, "../data") + "/users.json";
+    const readFile = async () => {
+      try {
+        const data = await fs.promises.readFile(filePath, "utf8");
+        return data;
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    readFile()
+      .then((res) => JSON.parse(res))
+      .then((res) => {
+        return reply.code(201).send({
+          order_number: "dhhdd",
+          res,
+        });
       })
-      .on("end", function () {
-        console.log(data);
+      .catch((err) => {
+        console.log(err);
       });
-  } catch (err) {}
+  } catch (err) {
+    return reply.code(400).send(err);
+  }
 }
 
 module.exports = { generateCart };
--- src/schemas/cart.schema.js
+++ src/schemas/cart.schema.js
@@ -4,27 +4,11 @@ const new_cart_schema ={
       properties:{
         user_id:{
           type:"string",
-          minLength:5
+          minLength:1
         }
       }
     },
-    response:{
-      201:{
-        type:"object",
-        properties:{
-          order_number:{type:"string"},
-          data:{
-            type:"object",
-          }
-        }
-      },
-    //   400:{
-    //     type:"object",
-    //     properties:{
-    //       error:{type:"string"}
-    //     }
-    //   }
-    }
+  
 }
 
 module.exports ={new_cart_schema}
