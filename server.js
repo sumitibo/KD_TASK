0dc25444cf5ccd22343c48cd1f00ee44d8d1b01a
Sumit Kumar <sumit.kumar@ibo.com>
2022-08-24 17:39:01 +0530

    Connected to postgres

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const fastify = require("fastify");
-const port = process.env.PORT || 7441;
+const {client} = require("./src/config/db");
+const fp = require("fastify-plugin");
+const port = 7441;
 const cartRoutes = require("./src/routes/cart.routes");
 const { checkAuthentication } = require("./src/hooks/authentication");
+
+
 
 const start = async () => {
   try {
     let app = fastify({ logger: false });
 
     app.register(cartRoutes);
+    app.decorate('client',client);
     app.addHook("onRequest", checkAuthentication);
-
+    client.connect();
     await app.listen({ port });
-
     console.log(`Server is listening on port ${port}`);
   } catch (err) {
     console.log(err.message);
--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -32,9 +32,11 @@ function updateQuantity(req, reply) {
   }
 }
 
-function getCartDetails(req, reply) {
+async function getCartDetails(req, reply) {
   try{
-
+    const query = await this.client.query("SELECT * FROM TESTING")
+    console.log(query)
+    return reply.code(200).send(query)
   }catch(err){
     return reply.code(400).send(err);
   }
